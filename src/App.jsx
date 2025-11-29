b5c76db73c4851262bd4064874317ccdf5ec0d55
hafsahbb <hafsahhbb@gmail.com>
2025-11-29 02:36:41 +0100

    Create App component for course registration system

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import RegistrationForm from './RegistrationForm';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'Arial' }}>
+      <h1>Course Registration System</h1>
+      <RegistrationForm />
+    </div>
+  );
+}
+
+export default App;
